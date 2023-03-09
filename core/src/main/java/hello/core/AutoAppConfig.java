9d565451f184e5b5cd367f45ebc4693125dd3643
bumgyu <soul1902@naver.com>
2023-03-09 23:51:38 +0900

    필터 includeComponent, excludeComponent, 커스텀

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,8 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class,
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
         //Componet애노테이션이 붙은 클래스를 스프링 빈 등록에서 제외 Configuration에도 @Component가 붙어 있다
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
